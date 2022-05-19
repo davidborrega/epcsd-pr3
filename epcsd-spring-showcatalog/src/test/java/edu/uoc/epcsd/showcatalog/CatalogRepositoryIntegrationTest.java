8df7ffd71240a5eac0e1a8c0f4fa8b409c23f843
dborrega <dborrega@gmail.com>
2022-05-19 12:14:30 +0200

    Update tests

--- epcsd-spring-showcatalog/src/test/java/edu/uoc/epcsd/showcatalog/CatalogRepositoryIntegrationTest.java
+++ epcsd-spring-showcatalog/src/test/java/edu/uoc/epcsd/showcatalog/CatalogRepositoryIntegrationTest.java
@@ -1,4 +1,39 @@
 package edu.uoc.epcsd.showcatalog;
 
+import edu.uoc.epcsd.showcatalog.domain.Category;
+import edu.uoc.epcsd.showcatalog.domain.repository.CategoryRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CatalogRepositoryIntegrationTest {
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void testGetCategories() {
+        Category category = new Category();
+        category.setId(7L);
+        category.setName("Test Category Name");
+        category.setDescription("Test Category description");
+        testEntityManager.persistAndFlush(category);
+        Optional<Category> categoryReturn = categoryRepository.findCategoryById(7L);
+        assertThat(categoryReturn.get().getName()).isEqualTo(category.getName());
+    }
+
 }
